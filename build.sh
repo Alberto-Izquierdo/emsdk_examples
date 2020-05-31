e936bf496a624e25816cfcf21723e5da2d3a4bde
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-05-31 20:34:55 +0200

    Added setup script and build script modified.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 OUTPUT_DIR="target"
 mkdir ${OUTPUT_DIR} >/dev/null 2>&1
 emcc -std=c++17 src/test.cpp -o ${OUTPUT_DIR}/test.js -s "EXPORTED_FUNCTIONS=['_getNumber']" -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']"
+cp static/index.html target/
