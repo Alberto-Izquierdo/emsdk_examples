605294b072f5bb9120325b6aff4794bd1daa6b29
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-05-31 20:30:39 +0200

    Added simple example.

---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+extern "C"
+{
+    int getNumber()
+    {
+        if (int a = 2; a < 3)
+        {
+            return 10;
+        }
+        else
+        {
+            return 20;
+        }
+    }
+}
