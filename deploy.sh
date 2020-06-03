97614b2866457c0f4067baaa211f2e708e77a8e6
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-06-03 20:15:37 +0200

    Fixed deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
-echo "Building cpp examples"
+echo "Copying files from static directory to target directory"
 cp static/index.html target/
 cp static/cpp/index.html target/cpp/
+echo "Creating the HTML list from the tree directory"
 pushd cpp >/dev/null 2>&1
 LIST=""
 for directory in $(ls -1); do
-    cd ${directory}
-    cp -r target ../../target/cpp/${directory}/
+    echo "Adding example \"cpp/${directory}\" to list"
     LIST="${LIST}\n<li><a href="./${directory}/index.html">${directory}</a></li>"
-    cd ..
 done
-echo "Examples built correctly"
+echo "List built correctly"
 popd >/dev/null 2>&1
-echo $(pwd)
 sed -i "s|{to_replace}|${LIST}|g" target/cpp/index.html
